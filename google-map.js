104fe287d6e09600c72dbd13b63356c47a7a29ad
NAGA1729 <140480951+NAGA1729@users.noreply.github.com>
2024-02-12 14:16:30 +0530

    Add files via upload

---
+++ google-map.js
@@ -0,0 +1,62 @@
+
+var google;
+
+function init() {
+    // Basic options for a simple Google Map
+    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
+    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    // 39.399872
+    // -8.224454
+    
+    var mapOptions = {
+        // How zoomed in you want the map to start at (always required)
+        zoom: 7,
+
+        // The latitude and longitude to center the map (always required)
+        center: myLatlng,
+
+        // How you would like to style the map. 
+        scrollwheel: false,
+        styles: [
+            {
+                "featureType": "administrative.country",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    },
+                    {
+                        "hue": "#ff0000"
+                    }
+                ]
+            }
+        ]
+    };
+
+    
+
+    // Get the HTML DOM element that will contain your map 
+    // We are using a div with id="map" seen below in the <body>
+    var mapElement = document.getElementById('map');
+
+    // Create the Google Map using out element and options defined above
+    var map = new google.maps.Map(mapElement, mapOptions);
+    
+    var addresses = ['New York'];
+
+    for (var x = 0; x < addresses.length; x++) {
+        $.getJSON('https://www.google.com/maps/place/Magic+bus+India+foundation+Azure+Free+Training+%26+Certification,/@17.4647917,78.4500263,20z/data=!4m6!3m5!1s0x3bcb911f6612aabd:0xf1c8e23814ea9953!8m2!3d17.4647917!4d78.4500263!16s%2Fg%2F11v615j650?authuser=0&entry=ttu='+addresses[x]+'&sensor=false', null, function (data) {
+            var p = data.results[0].geometry.location
+            var latlng = new google.maps.LatLng(p.lat, p.lng);
+            new google.maps.Marker({
+                position: latlng,
+                map: map,
+                icon: 'loc.png'
+            });
+
+        });
+    }
+    
+}
+google.maps.event.addDomListener(window, 'load', init);
